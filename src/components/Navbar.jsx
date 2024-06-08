6d0a37afd2045a8a5ffbc7dd179e6ef206b821c4
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-06-08 19:32:18 -0400

    ActiveClass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import pokemon_logo from "../assets/img/pokemon_logo.svg";
 import '../App.css';
 
 const Navigation = () => {
-  const location = useLocation();
-
-  const isCurrentRoute = (route) => {
-    const isPokemonRoute = location.pathname.startsWith("/pokemones/");
-    return location.pathname === route || (route === "/pokemones" && isPokemonRoute);
-  };
-
-  const linkStyle = (route) => ({
-    color: isCurrentRoute(route) ? "red" : "white",
-  });
-
   return (
     <Navbar className="navbar-dark-custom" variant="dark">
       <Container className="justify-content-between">
@@ -26,13 +15,15 @@ const Navigation = () => {
           </NavLink>
         </Navbar.Brand>
         <div>
-          <NavLink to="/" className="ms-3 text-decoration-none text-shadow" style={linkStyle("/")}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "ms-3 text-decoration-none text-shadow active" : "ms-3 text-decoration-none text-shadow")}
+          >
             Home
           </NavLink>
           <NavLink
             to="/pokemones"
-            className="ms-3 text-decoration-none text-shadow"
-            style={linkStyle("/pokemones")}
+            className={({ isActive }) => (isActive ? "ms-3 text-decoration-none text-shadow active" : "ms-3 text-decoration-none text-shadow")}
           >
             Pokemones
           </NavLink>
